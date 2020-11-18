b9eeaf157f9b2a122b8ac6c27c0b435b48a8507d
jangky000 <jangky000@mme.dongguk.edu>
2020-11-18 13:41:25 +0900

    [chore] 상태코드 파일 추가
    응답 상태 코드 관리를 위한 상태코드 파일 추가
    mysql2 설치
    sequelize 설치

--- server/app.js
+++ server/app.js
@@ -9,10 +9,6 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
---
+++ server/config/statusCode.js
@@ -0,0 +1,8 @@
+exports.statusCode = {
+  OK: 200,
+  CREATED: 201,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
