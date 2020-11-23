8ff08c3dfea014d755e592067f89d10ffd31aecf
jaejeon <jayten.42@gmail.com>
2020-11-23 16:26:44 +0900

    [Feat] Bucket 모델 생성

---
+++ iOS/project04/project04/Model/Bucket.swift
@@ -0,0 +1,16 @@
+//
+//  Bucket.swift
+//  project04
+//
+//  Created by jaejeon on 2020/11/23.
+//
+
+import Foundation
+
+struct Bucket: Hashable {
+    enum Section {
+        case todo
+    }
+    
+    let title: String
+}
