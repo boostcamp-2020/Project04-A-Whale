a8a8111c637c9a344daa4a4dca2c7f1998f6c9b6
yeji <yeji9175@naver.com>
2020-11-23 17:50:05 +0900

    [feat] router 기능 추가
    path에 따른 경로 분리를 위한 router 기능 추가

--- sidebar/src/App.js
+++ sidebar/src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router } from 'react-router-dom';
+import Routes from 'routes';
 
 const App = () => {
-  const clickHandler = () => {
-    whale.tabs.executeScript({
-      code: 'const bodyText = document.querySelector("body").innerText;alert(bodyText);',
-    });
-  };
   return (
-    <div className="App">
-      <h1>올해는 꼭</h1>
-      <button type="button" onClick={clickHandler}>
-        버튼
-      </button>
-    </div>
+    <>
+      <Router>
+        <Routes />
+      </Router>
+    </>
   );
 };
 
