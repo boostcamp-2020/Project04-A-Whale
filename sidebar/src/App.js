09ecb58e53de0fe054a065e6ba5c7b34b8e4557e
mistercle <mistercle@gmail.com>
2020-11-24 12:16:11 +0900

    [chore] reducer 설정
    reducer 설정 및 store 설정

--- sidebar/src/App.js
+++ sidebar/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
-import Routes from 'routes';
+import Routes from './routes';
 
 const App = () => {
   return (
--- sidebar/src/index.js
+++ sidebar/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
