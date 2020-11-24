02ed530c21a5f6adefd9d648b6dcb045ab9ac5f9
mistercle <mistercle@gmail.com>
2020-11-25 01:17:02 +0900

    [Feat] StyleButton 클릭이벤트 추가
    StyleButton에 클릭이벤트를 가질수있도록 기능 추가

--- sidebar/src/components/UI/atoms/StyledButton.js
+++ sidebar/src/components/UI/atoms/StyledButton.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
-const StyledButton = ({type, style, variant, content}) => {
+const StyledButton = ({type, style, variant, content, onClickHandler}) => {
     const useStyles = makeStyles({
         root: style,
     });
 
     const classes = useStyles();
     // 순수 텍스트만 있는 버튼의 경우
-    if(type === "Text") return (<Button className={classes.root} variant={variant}>{content}</Button>);
+    if(type === "Text") return (<Button className={classes.root} variant={variant} onClick={onClickHandler}>{content}</Button>);
     // 아이콘 버튼의 경우
-    else if(type === "Icon") return (<Button className={classes.root} aria-label={Variant}>{content}</Button>);
+    else if(type === "Icon") return (<Button className={classes.root} aria-label={variant} onClick={onClickHandler}>{content}</Button>);
 
 }
 
