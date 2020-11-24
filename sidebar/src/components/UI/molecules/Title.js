742e7cdd36d22dcbb8cf3beb789efda375428851
mistercle <mistercle@gmail.com>
2020-11-25 00:35:29 +0900

    [Feat] Title molecule 구현
    목표 달성페이지의 제목으로 쓰일 Title molecule 구현

---
+++ sidebar/src/components/UI/molecules/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Span from '../atoms/Span';
+
+const Title = (TitleStyle, content) => {
+    return (
+        <Span style={TitleStyle} content={content} />
+    );
+}
+
+export default Title;
