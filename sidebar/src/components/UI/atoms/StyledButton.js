75170c41a3d0ca76670a55c329dbb6a799ea9a3c
mistercle <mistercle@gmail.com>
2020-11-25 00:29:03 +0900

    [Feat] StyleButton 기능 추가
    StyleButton을 아이콘 버튼으로도 사용할수 있도록 수정

--- sidebar/src/components/UI/atoms/StyledButton.js
+++ sidebar/src/components/UI/atoms/StyledButton.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
-const StyledButton = ({style, variant, content}) => {
+const StyledButton = ({type, style, variant, content}) => {
     const useStyles = makeStyles({
         root: style,
     });
 
     const classes = useStyles();
-    return (
-        <Button className={classes.root} variant={variant}>{content}</Button>
-    );
+    // 순수 텍스트만 있는 버튼의 경우
+    if(type === "Text") return (<Button className={classes.root} variant={variant}>{content}</Button>);
+    // 아이콘 버튼의 경우
+    else if(type === "Icon") return (<Button className={classes.root} aria-label={Variant}>{content}</Button>);
 
 }
 
