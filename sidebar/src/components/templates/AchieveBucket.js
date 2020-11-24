c6ec5d4b5a735215c3db2a5720caf024cc8987c1
jangky000 <jangky000@mme.dongguk.edu>
2020-11-25 01:41:32 +0900

    [feat] 목표 달성 소감 작성 페이지 라우트 추가 templates에 레이아웃 추가

---
+++ sidebar/src/components/templates/AchieveBucket.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Hamburger from '../UI/atoms/Hamburger/Hamburger';
+import WriteTextArea from '../UI/organisms/WritingTab/WritingTab';
+
+const AchieveBucket = () => {
+  return (
+    <Layout>
+      <TopFixed>
+        <Hamburger />
+      </TopFixed>
+      <Page>
+        <div className="pageTitle">
+          <h1>목표달성 소감</h1>
+        </div>
+        <div className="bucketInfo">
+          <h2>#01 부스트캠프 수료</h2>
+          <p>멤버십을 훌륭하게 해내고 싶습니다. </p>
+          <p>from 2020.07.27</p>
+        </div>
+        <div className="writeImpression">
+          <WriteTextArea placeholder="목표 달성 소감을 남겨주세요." />
+        </div>
+      </Page>
+    </Layout>
+  );
+};
+
+const Layout = styled.div`
+  position: relative;
+`;
+
+const TopFixed = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+`;
+
+const Page = styled.div`
+  width: 80vw;
+  max-width: calc(100% - 140px);
+  margin: 0 auto;
+  margin-top: 50px;
+`;
+
+export default AchieveBucket;
--- sidebar/src/routes.js
+++ sidebar/src/routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import pathURI from './constants/path';
+import AchieveBucket from './components/templates/AchieveBucket';
 
 const routes = () => (
   <Switch>
-    <Route exact path={pathURI.home} />
+    <Route exact path={pathURI.home} component={AchieveBucket} />
     <Route exact path={pathURI.login} />
     <Route exact path={pathURI.register} />
   </Switch>
