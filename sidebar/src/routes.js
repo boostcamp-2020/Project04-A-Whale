7ea3370682a1a353693e6958bd6a122fbc7deac8
yeji <yeji9175@naver.com>
2020-11-24 21:36:05 +0900

    [fix] routes 수정
    MyBucketListPage로 이동하기 위한 component 속성 지정

--- sidebar/src/routes.js
+++ sidebar/src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import MyBucketListPage from './components/pages/MyBucketListPage';
 import pathURI from './constants/path';
 
 const routes = () => (
   <Switch>
-    <Route exact path={pathURI.home} />
-    <Route exact path={pathURI.login} />
-    <Route exact path={pathURI.register} />
+    <Route exact path={pathURI.home} component={MyBucketListPage} />
+    <Route path={pathURI.login} />
+    <Route path={pathURI.register} />
   </Switch>
 );
 
