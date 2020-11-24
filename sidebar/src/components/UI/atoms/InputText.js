3a1296356e670815e07a22baadbd1b365482bc65
mistercle <mistercle@gmail.com>
2020-11-25 00:21:31 +0900

    [Fix] 오타수정
    InputText 함수명 오타 수정

--- sidebar/src/components/UI/atoms/InputText.js
+++ sidebar/src/components/UI/atoms/InputText.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 
-const StyledButton = ({style, variant, label, defaultValue}) => {
+const InputText = ({style, variant, label, defaultValue}) => {
     const useStyles = makeStyles({
         root: style,
     });
@@ -14,4 +14,4 @@ const StyledButton = ({style, variant, label, defaultValue}) => {
 
 }
 
-export default StyledButton;
+export default InputText;
