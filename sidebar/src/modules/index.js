01708a3008fa3b249895d3c1a794ed614e88e14e
yeji <yeji9175@naver.com>
2020-11-24 21:47:16 +0900

    [fix] 오류 수정
    React.StrictMode 삭제
    modules/index.js combineReducers인자로 리듀서 지정

--- sidebar/src/modules/index.js
+++ sidebar/src/modules/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import todos from './todos';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  todos,
+});
 
 export default rootReducer;
