639eac3b123966d1d5661cee039556040ab9c9ab
mistercle <mistercle@gmail.com>
2020-11-25 01:18:10 +0900

    [Feat] InputText InputProps 추가
    InputText에 여러기능을 추가할 수 있도록 InputProps 옵션 추가

--- sidebar/src/components/UI/atoms/InputText.js
+++ sidebar/src/components/UI/atoms/InputText.js
@@ -2,14 +2,20 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 
-const InputText = ({style, variant, label, defaultValue}) => {
+const InputText = ({style, variant, label, defaultValue, onChangeHandler, InputProps}) => {
     const useStyles = makeStyles({
         root: style,
     });
 
     const classes = useStyles();
     return (
-        <TextField className={classes.root} variant={variant} label={label} defaultValue={defaultValue} />
+        <TextField 
+        className={classes.root} 
+        variant={variant} 
+        label={label} 
+        defaultValue={defaultValue} 
+        onChange={onChangeHandler}
+        InputProps={InputProps} />
     );
 
 }
