ba53e10494cc15ddf69ff8dd37f4595ab438a118
mistercle <mistercle@gmail.com>
2020-11-25 10:09:45 +0900

    [Feat] BucketContents 사이즈 조정
    Material UI의 TextField 크기 조정방식이 잘못되어서 이를 변경

--- sidebar/src/components/UI/organisms/BucketContents.js
+++ sidebar/src/components/UI/organisms/BucketContents.js
@@ -3,26 +3,25 @@ import styled from 'styled-components';
 import BucketInputText from '../molecules/BucketInputText';
 
 const BucketContentsWrapper = styled.div`
-    display: flex;
+  display: flex;
+  flex-direction: column;
 `;
 
 const titleStyle = {
-    width: "100%",
-    height: "400px",
-} 
+  width: '100%',
+};
 
 const decriptionStyle = {
-    width: "100%",
-    height: "800px",
-} 
+  width: '100%',
+};
 
 const BucketContents = () => {
-    return (
-        <BucketContentsWrapper>
-            <BucketInputText style={titleStyle} label="목표 Title 작성" />
-            <BucketInputText style={decriptionStyle} label="목표 Description 작성" />
-        </BucketContentsWrapper>
-    );
-}
+  return (
+    <BucketContentsWrapper>
+      <BucketInputText style={titleStyle} label="목표 Title 작성" />
+      <BucketInputText style={decriptionStyle} label="목표 Description 작성" />
+    </BucketContentsWrapper>
+  );
+};
 
 export default BucketContents;
