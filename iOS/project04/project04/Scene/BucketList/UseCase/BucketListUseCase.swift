4e0966261f8786fa52de393b6963c488a60dfdac
jaejeon <jayten.42@gmail.com>
2020-11-26 11:18:23 +0900

    [Feat] BucketListUseCase 구현

--- iOS/project04/project04/Scene/BucketList/UseCase/BucketListUseCase.swift
+++ iOS/project04/project04/Scene/BucketList/UseCase/BucketListUseCase.swift
@@ -10,20 +10,28 @@ import Foundation
 class BucketListUseCase: ListUseCase {
     typealias Item = Bucket
     
+    let repository: BucketListRepository
+    
+    init(repository: BucketListRepository) {
+        self.repository = repository
+    }
+    
     func fetch(completion: @escaping ([Bucket]) -> Void) {
-        
+        repository.fetchBucketList { (list) in
+            completion(list)
+        }
     }
     
     func append(_ element: Bucket) {
-        
+        repository.appendDetailList(element)
     }
     
     func remove(at index: Int) {
-        
+        repository.removeDetailList(at: index)
     }
     
     func revise(at index: Int, element: Bucket) {
-        
+        repository.reviseDetailList(at: index, element: element)
     }
     
     
