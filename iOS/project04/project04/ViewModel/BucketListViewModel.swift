7f88442527c00ac4bf4ba80290869c3758632dbd
jaejeon <jayten.42@gmail.com>
2020-11-26 11:33:33 +0900

    [Feat] ViewModle, UseCase, Repository 의존성 주입

--- iOS/project04/project04/ViewModel/BucketListViewModel.swift
+++ iOS/project04/project04/ViewModel/BucketListViewModel.swift
@@ -18,12 +18,17 @@ class BucketListViewModel {
         (self.buckets?[.todo]?.count ?? 0) + (self.buckets?[.done]?.count ?? 0)
     }
     
+    let useCase: BucketListUseCase
+    
     private var handler: ([Bucket.Section: [Bucket]]?) -> Void
     
-    init(with buckets: [Bucket.Section: [Bucket]], handler: @escaping ([Bucket.Section: [Bucket]]?) -> Void) {
-        self.buckets = buckets
+    init(useCase: BucketListUseCase, handler: @escaping ([Bucket.Section: [Bucket]]?) -> Void) {
         self.handler = handler
-        handler(buckets)
+        self.useCase = useCase
+        self.useCase.fetch { [weak self] list in
+            let buckets = [Bucket.Section.todo: list]
+            self?.buckets = buckets
+        }
     }
     
     func append(bucket: Bucket) {
