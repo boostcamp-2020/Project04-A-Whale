3181f0babcb0f287af815c0fd500644fde359a06
jaejeon <jayten.42@gmail.com>
2020-11-26 10:57:43 +0900

    [Feat] BucketListUseCase 생성

---
+++ iOS/project04/project04/Scene/BucketList/UseCase/BucketListUseCase.swift
@@ -0,0 +1,31 @@
+//
+//  BucketListUseCase.swift
+//  project04
+//
+//  Created by jaejeon on 2020/11/26.
+//
+
+import Foundation
+
+class BucketListUseCase: ListUseCase {
+    typealias Item = Bucket
+    
+    func fetch(completion: @escaping ([Bucket]) -> Void) {
+        
+    }
+    
+    func append(_ element: Bucket) {
+        
+    }
+    
+    func remove(at index: Int) {
+        
+    }
+    
+    func revise(at index: Int, element: Bucket) {
+        
+    }
+    
+    
+   
+}
