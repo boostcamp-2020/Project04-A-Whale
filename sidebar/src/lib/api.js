5bed4ae6f2d1ceab42bf4cfd67ad8d6228e5a746
DESKTOP-T1A999T\miste <mistercle@gmail.com>
2020-11-30 22:28:36 +0900

    [refactor] 린트 적용

--- sidebar/src/lib/api.js
+++ sidebar/src/lib/api.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
-export const createBucket = ( title, description, details, ref) => axios.post('/api/buckets', {
-    title: title,
-    description: description,
-    details: details,
-    ref: ref,
-});
+export const createBucket = (title, description, details, ref) =>
+  axios.post('http://localhost:8000/api/buckets', {
+    title,
+    description,
+    details,
+    ref,
+  });
 
-export const getPresets = ( keyword ) => axios.get(`/api/buckets/presets?keyword=${keyword}`);
+export const getPresets = (keyword) =>
+  axios.get(`http://localhost:8000/api/buckets/presets?keyword=${keyword}`);
 
-export const getDetails = ( bucketNo ) => axios.get(`/api/buckets/${bucketNo}/details`);
+export const getDetails = (bucketNo) =>
+  axios.get(`http://localhost:8000/api/buckets/${bucketNo}/details`);
