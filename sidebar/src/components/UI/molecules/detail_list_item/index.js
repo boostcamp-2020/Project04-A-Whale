b72ffaa2961d10087aae8af8fd6957610f9805a4
DESKTOP-T1A999T\miste <mistercle@gmail.com>
2020-12-01 11:56:36 +0900

    [Feat] 기한 날짜 지정 구현
    각 세부사항의 기한 날짜를 지정할수 있도록 구현

--- sidebar/src/components/UI/molecules/detail_list_item/index.js
+++ sidebar/src/components/UI/molecules/detail_list_item/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
 import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
 import DateRangeIcon from '@material-ui/icons/DateRange';
 import Span from '../../atoms/span/Span';
 import StyledButton from '../../atoms/styled_button/StyledButton';
+import InputText from '../../atoms/input_text';
 import { removeDetailAction } from '../../../../modules/actions/createbucket';
 
 const DetailListItemWrapper = styled.div`
@@ -17,13 +19,14 @@ const DetailListItemWrapper = styled.div`
   border-left: 0px;
   border-style: ridge;
   padding: 15px;
+  font-size: 14px;
 `;
 
-const CalenderIcon = {
+const DatePicker = {
   position: 'absolute',
   right: '12%',
   padding: '1px',
-  height: '20px',
+  margin: '-5px',
 };
 
 const RemoveIcon = {
@@ -43,14 +46,21 @@ const DetailListItem = ({ detail, removeDetailActionConnect }) => {
     removeDetailActionConnect(detail);
   };
 
+  const dateChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+
   const content = (
     <DetailListItemWrapper>
       <Span content={detail} />
-      <StyledButton
-        type="Icon"
-        style={CalenderIcon}
-        variant="add detail"
-        content={<DateRangeIcon />}
+      <InputText
+        style={DatePicker}
+        type="date"
+        onChangeHandler={dateChangeHandler}
+        defaultValue="2020-12-25"
+        InputLabelProps={{
+          shrink: true,
+        }}
       />
       <StyledButton
         type="Icon"
