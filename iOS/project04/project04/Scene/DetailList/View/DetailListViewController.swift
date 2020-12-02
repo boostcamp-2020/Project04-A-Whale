c72dd1452f0d530a7febb4b0d534cc66b65876c9
NamKiBeom <nkb5813@naver.com>
2020-12-02 22:18:04 +0900

    [feat] detail done swipe 버튼 추가, detail 수정 기능 구현

--- iOS/project04/project04/Scene/DetailList/View/DetailListViewController.swift
+++ iOS/project04/project04/Scene/DetailList/View/DetailListViewController.swift
@@ -36,22 +36,8 @@ class DetailListViewController: UIViewController {
         configureDataSource()
         
         collectionViewModel?.listDidChange = { [weak self] _ in
-            var snapshot = NSDiffableDataSourceSnapshot<Detail.Section, Detail>()
-            let sections: [Detail.Section] = [.graph, .feel, .todo, .done]
-            snapshot.appendSections(sections)
-            snapshot.appendItems([], toSection: .graph)
-            snapshot.appendItems([], toSection: .feel)
-            snapshot.appendItems(self?.collectionViewModel?.list[.todo] ?? [], toSection: .todo)
-            snapshot.appendItems(self?.collectionViewModel?.list[.done] ?? [], toSection: .done)
-            self?.dataSource.apply(snapshot, animatingDifferences: false)
+            self?.updateList()
         }
-        
-        collectionView.register(UINib(nibName: "DetailSectionHeaderView", bundle: nil),
-                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                                withReuseIdentifier: DetailSectionHeaderView.description())
-        collectionView.register(UINib(nibName: "DetailSectionImpressionHeaderView", bundle: nil),
-                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                                withReuseIdentifier: DetailSectionImpressionHeaderView.description())
         
         collectionViewModel?.listFetchAction()
     }
@@ -61,18 +47,28 @@ class DetailListViewController: UIViewController {
     }
 }
 
-extension DetailListViewController: UICollectionViewDelegate {
+extension DetailListViewController {
     private func createLayout() -> UICollectionViewLayout {
         var config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
         config.headerMode = .supplementary
-        config.trailingSwipeActionsConfigurationProvider = { (indexPath) -> UISwipeActionsConfiguration in
+        config.trailingSwipeActionsConfigurationProvider = { [weak self] indexPath in
+            let sectionIdentifier = self?.dataSource?.snapshot().sectionIdentifiers[indexPath.section].rawValue
+            guard sectionIdentifier == "todo" else { return nil }
+            
             let deleteAction = UIContextualAction(style: .destructive,
                                                   title: "Delete",
                                                   handler: { [weak self] _, _, completion in
                                                     self?.collectionViewModel?.listDeleteAction(at: indexPath.item)
                                                     completion(true)
                                                   })
-            return UISwipeActionsConfiguration(actions: [deleteAction])
+            
+            let doneAction = UIContextualAction(style: .normal,
+                                                title: "done",
+                                                handler: { [weak self] _, _, completion in
+                                                    self?.collectionViewModel?.listStatusReviseAction(at: indexPath.item)
+                                                })
+            
+            return UISwipeActionsConfiguration(actions: [doneAction, deleteAction])
         }
         
         return UICollectionViewCompositionalLayout.list(using: config)
@@ -80,6 +76,7 @@ extension DetailListViewController: UICollectionViewDelegate {
     
     private func configureHierarchy() {
         collectionView.collectionViewLayout = createLayout()
+        collectionView.delegate = self
     }
     
     private func configureDataSource() {
@@ -97,15 +94,19 @@ extension DetailListViewController: UICollectionViewDelegate {
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
         }
         
+        configureHeaderView()
+    }
+    
+    private func configureHeaderView() {
         dataSource.supplementaryViewProvider = { [weak self]
             (collectionView, kind, indexPath) -> UICollectionReusableView? in
+            let sectionIdentifier = self?.dataSource?.snapshot().sectionIdentifiers[indexPath.section].rawValue
             if kind == UICollectionView.elementKindSectionHeader {
-                if indexPath.section > 1 || indexPath.section == 0 {
+                if sectionIdentifier != "feel" {
                     guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DetailSectionHeaderView.description(), for: indexPath) as? DetailSectionHeaderView else {
                         return nil
                     }
-                    let sectionIdentifier = self?.dataSource?.snapshot().sectionIdentifiers[indexPath.section]
-                    headerView.titleLabel.text = sectionIdentifier?.rawValue
+                    headerView.titleLabel.text = sectionIdentifier
                     return headerView
                 } else {
                     guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DetailSectionImpressionHeaderView.description(), for: indexPath) as? DetailSectionImpressionHeaderView else {
@@ -115,6 +116,44 @@ extension DetailListViewController: UICollectionViewDelegate {
                 }
             }
             return nil
+        }
+        
+        collectionView.register(UINib(nibName: "DetailSectionHeaderView", bundle: nil),
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: DetailSectionHeaderView.description())
+        collectionView.register(UINib(nibName: "DetailSectionImpressionHeaderView", bundle: nil),
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: DetailSectionImpressionHeaderView.description())
+    }
+    
+    private func updateList() {
+        var snapshot = NSDiffableDataSourceSnapshot<Detail.Section, Detail>()
+        var sections: [Detail.Section] = [.todo, .done]
+        
+        if bucket?.status == "A" {
+            sections.insert(.feel, at: 0)
+            sections.insert(.graph, at: 0)
+            snapshot.appendSections(sections)
+            snapshot.appendItems([], toSection: .graph)
+            snapshot.appendItems([], toSection: .feel)
+        } else {
+            snapshot.appendSections(sections)
+        }
+        
+        snapshot.appendItems(collectionViewModel?.list[.todo] ?? [], toSection: .todo)
+        snapshot.appendItems(collectionViewModel?.list[.done] ?? [], toSection: .done)
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
+
+extension DetailListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let sectionIdentifier = self.dataSource?.snapshot().sectionIdentifiers[indexPath.section].rawValue
+        if sectionIdentifier == "todo" {
+            coordinator?.presentDetailListAdd(navigationController,
+                                              viewModel: collectionViewModel,
+                                              detail: collectionViewModel?.list[.todo]?[indexPath.item],
+                                              index: indexPath.item)
         }
     }
 }
