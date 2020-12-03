726686d9eb5a4483bc8096621763b5ba14667dc4
NamKiBeom <nkb5813@naver.com>
2020-12-03 16:12:59 +0900

    [feat] todo Section Collection Header View 추가(reuse 되는 상황이 나오지 않게 하기 위해)

---
+++ iOS/project04/project04/Scene/DetailList/View/SectionHeaderView/DetailSectionHeaderTodoView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailSectionHeaderTodoView.swift
+//  project04
+//
+//  Created by 남기범 on 2020/12/03.
+//
+
+import UIKit
+
+class DetailSectionHeaderTodoView: UICollectionReusableView {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
