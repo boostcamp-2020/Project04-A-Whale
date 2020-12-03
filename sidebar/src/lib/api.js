942421510c022958ba4f07f58a7bf7f4622a3c77
jangky000 <jangky000@mme.dongguk.edu>
2020-12-03 23:31:29 +0900

    [fix] /api/objects에서 /api/upload로 라우터 이름 변경

--- sidebar/src/lib/api.js
+++ sidebar/src/lib/api.js
@@ -35,11 +35,11 @@ export const updateAchieves = ({ achieveNo, description }) =>
     description,
   });
 
-export const setObjectStorage = (file) => {
+export const uploadObjectStorage = (file) => {
   const formData = new FormData();
   formData.append('file', file);
   const config = {
     header: { 'content-type': 'multipart/form-data' },
   };
-  return axios.post(`/api/objects/`, formData, config);
+  return axios.post(`/api/upload/`, formData, config);
 };
