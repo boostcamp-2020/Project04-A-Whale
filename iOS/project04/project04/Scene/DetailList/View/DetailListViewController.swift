806846228ebd88f59e871ddeb57e8e8391c3f291
NamKiBeom <nkb5813@naver.com>
2020-12-03 16:20:52 +0900

    [feat] 달성 완료 handler bind

--- iOS/project04/project04/Scene/DetailList/View/DetailListViewController.swift
+++ iOS/project04/project04/Scene/DetailList/View/DetailListViewController.swift
@@ -39,7 +39,16 @@ class DetailListViewController: UIViewController {
             self?.updateList()
         }
         
+        collectionViewModel?.todoListCountCheck = { [weak self] viewModel in
+            self?.detailSuccessChecker(viewModel: viewModel)
+        }
+        
         collectionViewModel?.listFetchAction()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        detailSuccessChecker(viewModel: collectionViewModel)
     }
     
     @IBAction func detailAppendAction(_ sender: UIBarButtonItem) {
@@ -57,14 +66,13 @@ extension DetailListViewController {
             
             let deleteAction = UIContextualAction(style: .destructive,
                                                   title: "Delete",
-                                                  handler: { [weak self] _, _, completion in
+                                                  handler: { [weak self] _, _, _ in
                                                     self?.collectionViewModel?.listDeleteAction(at: indexPath.item)
-                                                    completion(true)
                                                   })
             
             let doneAction = UIContextualAction(style: .normal,
                                                 title: "done",
-                                                handler: { [weak self] _, _, completion in
+                                                handler: { [weak self] _, _, _ in
                                                     self?.collectionViewModel?.listStatusReviseAction(at: indexPath.item)
                                                 })
             
@@ -101,29 +109,61 @@ extension DetailListViewController {
         dataSource.supplementaryViewProvider = { [weak self]
             (collectionView, kind, indexPath) -> UICollectionReusableView? in
             let sectionIdentifier = self?.dataSource?.snapshot().sectionIdentifiers[indexPath.section].rawValue
+            
             if kind == UICollectionView.elementKindSectionHeader {
-                if sectionIdentifier != "feel" {
+                if sectionIdentifier == "done" {
                     guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DetailSectionHeaderView.description(), for: indexPath) as? DetailSectionHeaderView else {
                         return nil
                     }
                     headerView.titleLabel.text = sectionIdentifier
+                    
+                    headerView.successHandler = { [weak self] in
+                        let alert = UIAlertController(title: "달성 여부",
+                                                      message: "버킷을 달성처리 하시겠습니까? 달성 시, 달성 취소가 불가능 합니다.",
+                                                      preferredStyle: .alert)
+                        
+                        let successAction = UIAlertAction(title: "예", style: .default, handler: { [weak self] _ in
+                            // 소감 작성 페이지로
+                            // todo 추가 불가능하게
+                            // 소감 작성 완료 시 리로드 해줘야함 (소감 섹션, 그래프 섹션 추가)
+                            // bucket으로 돌아갈 때, todo에서 done으로 바뀌어야함
+                        })
+                        let cancelAction = UIAlertAction(title: "아니오", style: .cancel)
+                        alert.addAction(successAction)
+                        alert.addAction(cancelAction)
+                        
+                        self?.present(alert, animated: false, completion: nil)
+                    }
                     return headerView
-                } else {
+                } else if  sectionIdentifier == "feel" {
                     guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DetailSectionImpressionHeaderView.description(), for: indexPath) as? DetailSectionImpressionHeaderView else {
                         return nil
                     }
+                    return headerView
+                } else {
+                    guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DetailSectionHeaderTodoView.description(), for: indexPath) as? DetailSectionHeaderTodoView else {
+                        return nil
+                    }
+                    headerView.titleLabel.text = sectionIdentifier
                     return headerView
                 }
             }
             return nil
         }
         
+        registerCollectionHeaderView()
+    }
+    
+    private func registerCollectionHeaderView() {
         collectionView.register(UINib(nibName: "DetailSectionHeaderView", bundle: nil),
                                 forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
                                 withReuseIdentifier: DetailSectionHeaderView.description())
         collectionView.register(UINib(nibName: "DetailSectionImpressionHeaderView", bundle: nil),
                                 forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
                                 withReuseIdentifier: DetailSectionImpressionHeaderView.description())
+        collectionView.register(UINib(nibName: "DetailSectionHeaderTodoView", bundle: nil),
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: DetailSectionHeaderTodoView.description())
     }
     
     private func updateList() {
@@ -144,12 +184,29 @@ extension DetailListViewController {
         snapshot.appendItems(collectionViewModel?.list[.done] ?? [], toSection: .done)
         dataSource.apply(snapshot, animatingDifferences: false)
     }
+    
+    private func detailSuccessChecker(viewModel: DetailListViewModelProtocol?) {
+        if viewModel?.list[.todo]?.count == 0,
+           viewModel?.list[.done]?.count != 0 {
+            isHiddenHeaderSuccessButton(status: false)
+        } else {
+            isHiddenHeaderSuccessButton(status: true)
+        }
+    }
+    
+    private func isHiddenHeaderSuccessButton(status: Bool) {
+        let headerViews = collectionView.visibleSupplementaryViews(ofKind: UICollectionView.elementKindSectionHeader)
+        let headerView = headerViews.filter { $0.isKind(of: DetailSectionHeaderView.self) }
+            .map { $0 as? DetailSectionHeaderView }
+            .filter { $0?.titleLabel.text == "done" }.first
+        headerView??.successButton.isHidden = status
+    }
 }
 
 extension DetailListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let sectionIdentifier = self.dataSource?.snapshot().sectionIdentifiers[indexPath.section].rawValue
-        if sectionIdentifier == "todo" {
+        if sectionIdentifier == "todo" && bucket?.status != "A" {
             coordinator?.presentDetailListAdd(navigationController,
                                               viewModel: collectionViewModel,
                                               detail: collectionViewModel?.list[.todo]?[indexPath.item],
