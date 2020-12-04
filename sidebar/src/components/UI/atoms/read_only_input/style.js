1ece35c8e286a80f97560ad804271c8db8d56ae6
yeji <yeji9175@naver.com>
2020-12-04 11:37:23 +0900

    [style] 달설 소감 css 수정

--- sidebar/src/components/UI/atoms/read_only_input/style.js
+++ sidebar/src/components/UI/atoms/read_only_input/style.js
@@ -2,7 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    border: '1px solid #eee',
+    marginTop: '10px',
+    border: '1px solid #ccc',
     borderRadius: '5px',
     padding: '10px',
   },
