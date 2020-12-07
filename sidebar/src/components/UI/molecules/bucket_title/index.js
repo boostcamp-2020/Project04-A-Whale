9f9d325f20b51338ea9fcb0c2dc79e3ee9ecd5c2
DESKTOP-T1A999T\miste <mistercle@gmail.com>
2020-12-07 23:23:51 +0900

    [Style] 검색창 삭제, 제목에 검색버튼 구현
    제목 입력란에 검색버튼을 구현하여 UX 개선

--- sidebar/src/components/UI/molecules/bucket_title/index.js
+++ sidebar/src/components/UI/molecules/bucket_title/index.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
+/* eslint-disable */
+import React, { useState } from 'react';
+import { IconButton, Input, InputAdornment, InputLabel, Modal } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import SearchGuide from '../search_guide';
 
 const BucketDescription = ({ style, label, value, onChangeHandler }) => {
+
+    const [isHover, setHover] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    const onClickHandler = () => {
+        setOpen(true);
+    }
+
+    const modalClose = () => {
+        setOpen(false);
+    }
+
+    const onHoverEvent = () => {
+        setHover(true)
+    }
+
+    const onLeaveEvent = () => {
+        setHover(false)
+    }
+
   return (
-    <TextField
-      style={style}
-      variant="filled"
-      label={label}
-      onChange={onChangeHandler}
-      value={value}
-    />
+    <>
+      <InputLabel>{label}</InputLabel>
+      <Input
+        style={style}
+        variant="filled"
+        onChange={onChangeHandler}
+        value={value}
+        endAdornment={
+            <>
+          <InputAdornment position="end">
+            <IconButton onClick={onClickHandler} onMouseOver={onHoverEvent} onMouseLeave={onLeaveEvent} >
+              <SearchIcon />
+            </IconButton>
+            <SearchGuide isHover={isHover}/>
+          </InputAdornment>
+          </>
+        }
+      />
+      <Modal>
+
+      </Modal>
+    </>
   );
 };
 
---
+++ sidebar/src/components/UI/molecules/search_guide/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import { Typography } from '@material-ui/core';
+import useStyle from './style';
+import useMousePosition from '../../../../lib/useMousePosition';
+
+const SearchGuide = ({ isHover }) => {
+  const { posX, posY } = useMousePosition();
+  const isVisible = isHover ? 'block' : 'none';
+  const classes = useStyle({ posX, posY, isVisible });
+
+  return (
+    <Card className={classes.root}>
+      <Typography className={classes.top}>다른 사람들의 목표가 궁금하신가요?</Typography>
+      <Typography className={classes.middle}>
+        검색기능을 이용하여 다른 사람들의 목표를 볼수있고
+      </Typography>
+      <Typography className={classes.low}>불러와서 내 마음대로 수정할 수 있습니다.</Typography>
+    </Card>
+  );
+};
+export default SearchGuide;
