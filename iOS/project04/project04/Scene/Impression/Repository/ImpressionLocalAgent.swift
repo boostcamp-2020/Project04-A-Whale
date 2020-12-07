66c4aaff2cc06b066a0d6f03e4b4994fe2bb2e2f
NamKiBeom <nkb5813@naver.com>
2020-12-07 22:32:03 +0900

    [feat] 소감 repository 구현

---
+++ iOS/project04/project04/Scene/Impression/Repository/ImpressionLocalAgent.swift
@@ -0,0 +1,59 @@
+//
+//  ImpressionLocalAgent.swift
+//  project04
+//
+//  Created by 남기범 on 2020/12/07.
+//
+
+import Foundation
+import RealmSwift
+
+protocol ImpressionLocalAgentProtocol {
+    func fetch(bucketNo: Int) -> RealmImpression?
+    func save(text: String)
+    func edit(element: RealmImpression, text: String)
+}
+
+class ImpressionLocalAgent {
+    private var bucketNo: Int
+    
+    init(bucketNo: Int) {
+        self.bucketNo = bucketNo
+    }
+    
+    func fetch(bucketNo: Int) -> RealmImpression? {
+        do {
+            let realm = try Realm()
+            let object = realm.objects(RealmImpression.self).filter("#bucketNo == \(bucketNo)").first
+            return object
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    func save(text: String) {
+        do {
+            let realm = try Realm()
+            
+            try realm.write {
+                let object = realm.objects(RealmImpression.self).filter("#bucketNo == \(bucketNo)")
+                realm.delete(object)
+                let impression = RealmImpression(value: [text, bucketNo])
+                realm.add(impression)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func edit(element: RealmImpression?, text: String) {
+        do {
+            try Realm().write {
+                element?.text = text
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
