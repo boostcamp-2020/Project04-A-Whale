959fc8994cb307b7d387616ce6b64c1e29bbf953
NamKiBeom <nkb5813@naver.com>
2020-12-07 22:29:10 +0900

    [feat] 소감 텍스트 및 수정 버튼 handler 주입

--- iOS/project04/project04/Scene/DetailList/View/SectionHeaderView/DetailSectionImpressionHeaderView.swift
+++ iOS/project04/project04/Scene/DetailList/View/SectionHeaderView/DetailSectionImpressionHeaderView.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class DetailSectionImpressionHeaderView: UICollectionReusableView {
-
+    @IBOutlet weak var impressionLabel: UILabel!
+    var editHandler: (() -> ())?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
+    @IBAction func impressionEditAction(_ sender: UIButton) {
+        editHandler?()
+    }
 }
