3209d9894e04abfef1911e05a18d843226f92859
jangky000 <46799722+jangky000@users.noreply.github.com>
2020-12-08 10:16:09 +0900

    Delete useValidator.js

--- sidebar/src/hooks/useValidator.js
+++
@@ -1,7 +0,0 @@
-import React, { useState, useCallback } from 'react';
-
-export default useValidator = ({ initialState, validator }) => {
-  const [value, setValue] = useState(initialState);
-  const [isValid, setIsValid] = useState(true);
-  return [value, setValue, isValid];
-};
