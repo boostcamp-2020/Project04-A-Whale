a49ca39c074e22f463cc4fefd223e7b9010c9f85
jangky000 <jangky000@mme.dongguk.edu>
2020-12-08 23:50:00 +0900

    [feat] 입력값을 검증하는 커스텀 훅 추가

--- sidebar/src/hooks/useValidator.js
+++ sidebar/src/hooks/useValidator.js
@@ -1,7 +1,30 @@
-import React, { useState, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 
-export default useValidator = ({ initialState, validator }) => {
+const useValidator = (initialState, validationFunc1, message1, validationFunc2, message2) => {
   const [value, setValue] = useState(initialState);
-  const [isValid, setIsValid] = useState(true);
-  return [value, setValue, isValid];
+  const [ErrorMsg, setErrorMsg] = useState('');
+
+  const changeHandler = useCallback((e) => {
+    setValue(e.target.value, [value]);
+  });
+
+  let validHandler = null;
+  if (validationFunc1) {
+    validHandler = useCallback(async () => {
+      if (!validationFunc1(value)) {
+        setErrorMsg(message1);
+        return false;
+      }
+      if (validationFunc2 && !(await validationFunc2(value))) {
+        setErrorMsg(message2);
+        return false;
+      }
+      setErrorMsg('');
+      return true;
+    }, [value]);
+  }
+
+  return [value, changeHandler, ErrorMsg, validHandler, setErrorMsg];
 };
+
+export default useValidator;
