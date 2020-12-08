76931433da5491b94844e99ff9640e619190ac82
jaejeon <jayten.42@gmail.com>
2020-12-08 23:10:08 +0900

    Endpoint에 buckets 추가

--- iOS/project04/project04/Util/Enum/Endpoint.swift
+++ iOS/project04/project04/Util/Enum/Endpoint.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum Endpoint: String {
-    static let baseURL = "http://101.101.210.76:8000"
+    static let baseURL = "http://101.101.210.76:8000/api"
     case buckets
     
     var urlString: String {
