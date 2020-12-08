1743f3a901ff0ac3e988c5f2fbf0a37e40fe7d0c
jangky000 <jangky000@mme.dongguk.edu>
2020-12-08 23:51:27 +0900

    [feat] 회원가입 기능을 서버와 연결
    입력값 검증 및 중복 아이디 확인 가능

--- sidebar/src/components/templates/register/index.js
+++ sidebar/src/components/templates/register/index.js
@@ -19,8 +19,59 @@ import {
   RegisterBox,
 } from './style';
 
-const Login = () => {
+import useValidator from '../../../hooks/useValidator';
+import * as validator from '../../../lib/validator';
+
+const Login = ({ setRegisterInfo }) => {
   const classes = useStyles();
+
+  const [id, idChange, idErrorMsg, idCheck] = useValidator(
+    '',
+    validator.isID,
+    '아이디는 영문 숫자 조합 5~25자리.',
+    validator.duplicatedID,
+    '중복된 아이디입니다.'
+  );
+  const [password, passwordChange, passwordErrorMsg, passwordCheck] = useValidator(
+    '',
+    validator.isPassword,
+    '숫자, 특문 각 1회 이상, 영문은 2개 이상 사용하여 8자리 이상 입력.'
+  );
+
+  const [nickname, nicknameChange, nicknameErrorMsg, nicknameCheck] = useValidator(
+    '',
+    validator.isNickname,
+    '닉네임은 특수문자 제외 2자 이상'
+  );
+  const [description, descriptionChange] = useValidator('');
+
+  const [
+    passwordRepeat,
+    passwordRepeatChange,
+    passwordRepeatErrorMsg,
+    ,
+    setErrorMsg,
+  ] = useValidator('', null);
+
+  const passwordRepeatCheck = useCallback(() => {
+    if (password !== passwordRepeat) {
+      setErrorMsg('패스워드가 일치하지 않습니다.');
+      return false;
+    }
+    setErrorMsg('');
+    return true;
+  }, [password, passwordRepeat]);
+
+  const submitClickHandler = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (idCheck() && passwordCheck() && passwordRepeatCheck() && nicknameCheck()) {
+        setRegisterInfo({ id, password, nickname, description });
+      }
+    },
+    [id, password, passwordRepeat, nickname, description]
+  );
+
   const history = useHistory();
   const goBack = useCallback(() => history.goBack(), []);
 
@@ -74,22 +125,51 @@ const Login = () => {
       <RegisterLayout>
         <RegisterContainer>
           <RegisterBox>
-            <form>
-              <TextField className={classes.input} label="아이디" variant="outlined" />
+            <form onSubmit={submitClickHandler}>
+              <TextField
+                className={classes.input}
+                label="아이디"
+                variant="outlined"
+                onChange={idChange}
+                onBlur={idCheck}
+                error={idErrorMsg}
+                helperText={idErrorMsg}
+              />
               <TextField
                 className={classes.input}
                 label="패스워드"
                 type="password"
                 variant="outlined"
+                onChange={passwordChange}
+                onBlur={passwordCheck}
+                error={passwordErrorMsg}
+                helperText={passwordErrorMsg}
               />
               <TextField
                 className={classes.input}
                 label="패스워드 확인"
                 type="password"
                 variant="outlined"
+                onChange={passwordRepeatChange}
+                onBlur={passwordRepeatCheck}
+                error={passwordRepeatErrorMsg}
+                helperText={passwordRepeatErrorMsg}
               />
-              <TextField className={classes.input} label="닉네임" variant="outlined" />
-              <TextField className={classes.input} label="소개" variant="outlined" />
+              <TextField
+                className={classes.input}
+                label="닉네임"
+                variant="outlined"
+                onChange={nicknameChange}
+                onBlur={nicknameCheck}
+                error={nicknameErrorMsg}
+                helperText={nicknameErrorMsg}
+              />
+              <TextField
+                className={classes.input}
+                label="소개"
+                variant="outlined"
+                onChange={descriptionChange}
+              />
               <Button
                 className={classes.outlineButton}
                 variant="outlined"
