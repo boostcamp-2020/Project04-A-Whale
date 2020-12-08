0bb0e899439464ee73b58822fb67e1f822a449c4
NamKiBeom <nkb5813@naver.com>
2020-12-08 23:15:36 +0900

    [feat] url path 추가

--- iOS/project04/project04/Util/Enum/Endpoint.swift
+++ iOS/project04/project04/Util/Enum/Endpoint.swift
@@ -10,8 +10,10 @@ import Foundation
 enum Endpoint: String {
     static let baseURL = "http://101.101.210.76:8000"
     case buckets
+    case achieves = "api/achieves"
+    case details = "api/details"
     
     var urlString: String {
-        return Endpoint.baseURL + "/\(self)"
+        return Endpoint.baseURL + "/\(self.rawValue)"
     }
 }
