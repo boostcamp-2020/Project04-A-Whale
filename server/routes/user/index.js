6d597afd9e47560e96103a518e9e95bc3ca03cc3
DESKTOP-T1A999T\miste <mistercle@gmail.com>
2020-12-10 13:42:00 +0900

    [Feat] 사용자 no를 기반으로 검색할수있도록 API 구현

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -6,6 +6,7 @@ router.get('/search', controller.searchUsers);
 router.get('/:id', controller.isDuplicated);
 router.get('/', middlewares.jwtAuth, controller.getUsers);
 router.get('/info', controller.getUserInfo);
+router.get('/info/:no', controller.getUserInfoByNo);
 router.post('/', controller.setUser);
 router.post('/login', middlewares.localAuth, controller.login);
 
