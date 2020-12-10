63f1cc2974762fd922264c42cff9e49ce1aed4d8
jangky000 <jangky000@mme.dongguk.edu>
2020-12-10 23:24:41 +0900

    [feat] 환경설정 레이아웃 생성

---
+++ sidebar/src/components/pages/SettingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SettingLayout from '../templates/setting';
+import Header from '../UI/organisms/header';
+
+const SettingPage = () => {
+  return (
+    <>
+      <Header title="설정" />
+      <SettingLayout />
+    </>
+  );
+};
+
+export default SettingPage;
