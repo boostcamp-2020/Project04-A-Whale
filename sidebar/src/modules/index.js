56ed8424fa7eded5df885d07ca8e032f19930833
jangky000 <jangky000@mme.dongguk.edu>
2020-12-11 04:52:29 +0900

    [refactor] 소감 작성 페이지 관련 변수명 리팩토링

--- sidebar/src/modules/index.js
+++ sidebar/src/modules/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import createbucket from './createbucket';
-import acheiveState from './achieve';
+import acheive from './achieve';
 import buckets from './buckets';
 import loading from './loading';
 import details from './details';
@@ -9,7 +9,7 @@ import feeds from './feeds';
 
 const rootReducer = combineReducers({
   createbucket,
-  acheiveState,
+  acheive,
   buckets,
   loading,
   details,
