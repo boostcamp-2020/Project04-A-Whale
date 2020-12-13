2ab6a23f041b39ab4b1d1586d9f9494c1e14804a
DESKTOP-T1A999T\miste <mistercle@gmail.com>
2020-12-13 22:24:40 +0900

    [Refactor] 사용자 정보 템플릿 스타일 파일 분리

--- sidebar/src/components/templates/user_info_template/index.js
+++ sidebar/src/components/templates/user_info_template/index.js
@@ -1,18 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import UserInfo from '../../UI/organisms/user_info';
 import FollowButton from '../../UI/molecules/follow_button';
 import UserBuckets from '../../UI/organisms/user_buckets';
-
-const useStyles = makeStyles((theme) => ({
-  header: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  },
-}));
+import useStyles from './style';
 
 const UserInfoTemplate = ({ userNo }) => {
   const classes = useStyles();
---
+++ sidebar/src/components/templates/user_info_template/style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    header: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+  }));
+
+export default useStyles;
