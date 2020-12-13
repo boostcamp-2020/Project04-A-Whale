86282ae9072df94cc789ab7303390e5cf2dd3d11
DESKTOP-T1A999T\miste <mistercle@gmail.com>
2020-12-13 22:21:40 +0900

    [Refactor] 버킷 생성 템플릿 스타일 파일 리

--- sidebar/src/components/templates/bucket_create_template/index.js
+++ sidebar/src/components/templates/bucket_create_template/index.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
-import { makeStyles } from '@material-ui/core/styles';
 import BucketContents from '../../UI/organisms/bucket_contents';
 import CreateDetailList from '../../UI/organisms/create_detail_list';
 import Buttons from '../../UI/organisms/buttons';
-
-const BucketCreateTemplateWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 15px;
-`;
-
-const useStyles = makeStyles((theme) => ({
-  header: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  },
-}));
+import useStyles, { BucketCreateTemplateWrapper } from './style';
 
 const BucketCreateTemplate = () => {
   const classes = useStyles();
---
+++ sidebar/src/components/templates/bucket_create_template/style.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
+
+export const BucketCreateTemplateWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+`;
+
+const useStyles = makeStyles((theme) => ({
+    header: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+  }));
+
+export default useStyles;
