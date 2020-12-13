becd798d1942cd2856f341a3dd6300bc0a8058e1
DESKTOP-T1A999T\miste <mistercle@gmail.com>
2020-12-13 22:16:33 +0900

    [Refactor] 팔로우 리스트 템플릿 스타일 파일 분리

--- sidebar/src/components/templates/follow_list_template/index.js
+++ sidebar/src/components/templates/follow_list_template/index.js
@@ -1,17 +1,7 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import UserSearch from '../../UI/organisms/user_search';
 import FollowTabBar from '../../UI/organisms/follow_tab_bar';
-
-const useStyles = makeStyles((theme) => ({
-  header: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  },
-}));
+import useStyles from './style';
 
 const FollowListTemplate = ({ followed, following }) => {
   const classes = useStyles();
