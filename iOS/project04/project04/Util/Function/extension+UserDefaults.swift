1588c810a97bc1571aac528c53cc2925baa50fa6
NamKiBeom <nkb5813@naver.com>
2020-12-15 00:52:56 +0900

    [feat] widget 구현 - UserDefault widget과 공유하기 위한 extension 추가 - 프로젝트 그룹 설정(UserDefault 공유)

---
+++ iOS/project04/project04/Util/Function/extension+UserDefaults.swift
@@ -0,0 +1,15 @@
+//
+//  UserDefaults+extension.swift
+//  project04
+//
+//  Created by 남기범 on 2020/12/14.
+//
+
+import Foundation
+
+//extension UserDefaults {
+//    static var shared: UserDefaults {
+//        let appGroupId = "group.namkibeom.project04"
+//        return UserDefaults(suiteName: appGroupId)!
+//    }
+//}
