c8e57da4382dba7e3bc15d43c1bd692adf0665a9
jangky000 <jangky000@mme.dongguk.edu>
2020-12-15 23:28:13 +0900

    [feat] 로그인 시 알람 설정

--- sidebar/src/components/pages/LoginPage.js
+++ sidebar/src/components/pages/LoginPage.js
@@ -2,17 +2,39 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import Login from '../templates/login';
 import { userLogin } from '../../lib/api';
+import { updateDueDetailsAndAlarm } from '../../lib/alarm';
+import { getChromeLocalStorage } from '../../lib/chromeLocalStorage';
 
 const LoginPage = () => {
   const [loginInfo, setLoginInfo] = useState({ id: '', password: '' });
+  const history = useHistory();
   const resetLoginInfo = () => {
     setLoginInfo({ id: '', password: '' });
   };
-  const history = useHistory();
+  const alarmSetting = async () => {
+    try {
+      // 알람 추가하기
+      const keys = ['sw', 'alarm'];
+      const callback = async (items) => {
+        const { sw, alarm } = items;
+        console.log(sw, alarm);
+        if (sw.alarmOn) {
+          await updateDueDetailsAndAlarm(items, { sw, alarm });
+        }
+        return null;
+      };
+      getChromeLocalStorage(keys, callback);
+    } catch (error) {
+      console.log(error);
+      console.log('웨일 확장앱이 아닙니다. 알람을 설정하지 않습니다.');
+    }
+  };
+
   const axiosLogin = useCallback(async (body) => {
     const result = await userLogin(body);
     localStorage.setItem('accessToken', result.data.accessToken);
     resetLoginInfo();
+    await alarmSetting();
     history.replace('/');
   }, []);
 
